f24a19c509ed94a9f143dd08d2eda76782364428
Jyothishv7 <61043734+Jyothishv7@users.noreply.github.com>
2021-06-14 16:26:17 +0530

    Update solns.c

--- solns.c
+++ solns.c
@@ -1,2 +1,18 @@
 /* Enter your solutions in this file */
 #include <stdio.h>
+#include <cs50.h>
+
+int max(int [], int);
+
+int max(int ar[], int count)
+{
+  int highest=0;
+  for (int i=0;i<count;i++)
+  {
+    if(ar[i]>highest)
+    {
+      highest=ar[i];
+    }
+  }
+  return highest;
+}
