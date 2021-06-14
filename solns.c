8a2a9178cb86ccebe879c57b95a7d04796c68ed4
Jyothishv7 <61043734+Jyothishv7@users.noreply.github.com>
2021-06-14 16:36:47 +0530

    Update solns.c

--- solns.c
+++ solns.c
@@ -1,9 +1,10 @@
 /* Enter your solutions in this file */
 #include <stdio.h>
 
-
 int max(int [], int);
+int min(int [], int);
 
+/*highest num in array*/
 int max(int ar[], int count)
 {
   int highest=0;
@@ -15,4 +16,18 @@ int max(int ar[], int count)
     }
   }
   return highest;
+}
+
+/*lowest num in array*/
+int max(int ar[], int count)
+{
+  int lowest=-1;
+  for (int i=0;i<count;i++)
+  {
+    if(ar[i]<lowest)
+    {
+      lowest=ar[i];
+    }
+  }
+  return lowest;
 }
